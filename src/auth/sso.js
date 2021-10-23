449d7603bcb3db5581736b8e14f417fffa1b6893
Andres Garcia <jose.andres.gm29@gmail.com>
2021-10-23 12:47:35 -0500

    run last fix of sso

--- src/auth/sso.js
+++ src/auth/sso.js
@@ -7,7 +7,7 @@ class SSO {
       clientID: 'pZMfX5rRrut76LLRQN5UnALov7yqCeYj',
       audience: 'https://corvux.us.auth0.com/api/v2/',
       redirectUri: 'https://evnt.andresg.me/login',
-      //redirectUri: 'http://localhost:8080/login',
+      // redirectUri: 'http://localhost:8080/login',
       scope: 'openid email profile',
       responseType: 'token id_token',
     })
