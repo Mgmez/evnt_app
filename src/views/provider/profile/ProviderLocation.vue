9836c112fe5ecc5cc1ef5053d1b5cd53d34bf8b1
santiago villalobos <55002307+SanjeVicor@users.noreply.github.com>
2021-12-08 17:39:35 -0600

    google api integration

--- src/views/provider/profile/ProviderLocation.vue
+++ src/views/provider/profile/ProviderLocation.vue
@@ -4,27 +4,17 @@
       v-if="isMyProfile"
       class="text-primary"
     >Arrastra el icono a tu ubicacion</h6>
-    <l-map
-      :zoom="zoom"
-      :center="center"
-    >
-      <l-tile-layer :url="url" />
-      <l-marker
-        v-if="isMyProfile"
-        :lat-lng="markerLatLng"
-        :draggable="true"
-        @mouseleave="alert"
-      >
-        <l-popup>Estas aqui</l-popup>
-      </l-marker>
-      <l-marker
-        v-else
-        :lat-lng="markerLatLng"
-        :draggable="false"
-      >
-        <l-popup>Ubicacion de la empresa</l-popup>
-      </l-marker>
-    </l-map>
+
+    <b-form-input
+      id="pac-input"
+      class="controls"
+      type="text"
+      placeholder="Search Box"
+    />
+    <div
+      id="map"
+      style="width: 100%; height: 500px"
+    />
     <b-button-group>
       <b-button
         v-if="isMyProfile"
@@ -47,16 +37,15 @@
 <script>
 import BCardCode from '@core/components/b-card-code/BCardCode.vue'
 import {
-  LMap, LTileLayer, LMarker, LPopup,
-} from 'vue2-leaflet'
-import {
   BButton,
   BButtonGroup,
+  BFormInput,
 } from 'bootstrap-vue'
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
 import axios from 'axios'
 import { buildServiceUrl } from '@/constants/urls'
+import { Loader } from '@googlemaps/js-api-loader'
 import code from './code'
 // eslint-disable-next-line no-underscore-dangle
 delete Icon.Default.prototype._getIconUrl
@@ -70,13 +59,10 @@ Icon.Default.mergeOptions({
 })
 export default {
   components: {
-    LMap,
-    LTileLayer,
-    LPopup,
-    LMarker,
     BCardCode,
     BButton,
     BButtonGroup,
+    BFormInput,
   },
   props: {
     latitude: {
@@ -101,39 +87,129 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       zoom: 15,
       center: [Number(this.latitude), Number(this.longitude)],
-      markerLatLng: [Number(this.latitude), Number(this.longitude), { draggable: 'true' }],
       code,
       isMyProfile: false,
+      myLat: this.latitude,
+      myLong: this.longitude,
     }
   },
   beforeMount() {
     console.log('sadasdas')
-    console.log(this.latitude)
-    console.log(this.longitude)
+    console.log(this.myLat)
+    console.log(this.myLong)
     const sessionData = JSON.parse(localStorage.getItem('userData'))
     if (sessionData.data[0].type_id === this.userId) {
       this.isMyProfile = true
     }
-    if (this.latitude === null || this.altitude === null || this.latitude === 0 || this.altitude === 0) {
+    if (this.myLat === null || this.myLong === null || this.myLat === 0 || this.myLong === 0) {
       navigator.geolocation.getCurrentPosition(this.createCurrentPosition, this.showError)
     }
+  },
+  mounted() {
+    this.initMap()
   },
   methods: {
     alert(e) {
       console.log(e)
-      this.latitude = e.latlng.lat
-      this.longitude = e.latlng.lng
+      this.myLat = e.latlng.lat
+      this.myLong = e.latlng.lng
     },
     createCurrentPosition(pos) {
-      console.log('pos')
       console.log(pos)
-      this.latitude = pos.coords.latitude
-      this.altitude = pos.coords.altitude
-      this.longitude = pos.coords.longitude
+      this.myLat = pos.coords.latitude
+      this.myLong = pos.coords.longitude
       this.center = [pos.coords.latitude, pos.coords.longitude]
       this.markerLatLng = [pos.coords.latitude, pos.coords.longitude, { draggable: 'true' }]
+      this.initMap()
     },
-    getMyCurrentPosition() {
+    async initMap() {
+      console.log('object')
+      const loader = new Loader({
+        apiKey: 'AIzaSyAkp6YqJgpYVe2iDsTuXMxkfCNwzFSUOwk',
+        version: 'weekly',
+        libraries: ['places'],
+      })
+      const input = document.getElementById('pac-input')
+      const google = await loader.load()
+      // eslint-disable-next-line no-unused-vars
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(this.myLat, this.myLong),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 17,
+      })
+
+      const searchBox = new google.maps.places.SearchBox(input)
+
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input)
+      // Bias the SearchBox results towards current map's viewport.
+      map.addListener('bounds_changed', () => {
+        searchBox.setBounds(map.getBounds())
+      })
+
+      /**
+       * NOTA
+       * myMarker solo si el usuario tiene ubicacion guarda en la base de datos
+       */
+      let myMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.myLat, this.myLong),
+        draggable: true,
+      })
+      console.log('myMarker')
+      console.log(myMarker)
+
+      google.maps.event.addListener(myMarker, 'dragend', evt => {
+        this.myLat = evt.latLng.lat()
+        this.myLong = evt.latLng.lng()
+        console.log('change')
+        map.panTo(evt.latLng)
+      })
+      map.setCenter(myMarker.position)
+      myMarker.setMap(map)
+
+      // Listen for the event fired when the user selects a prediction and retrieve
+      // more details for that place.
+      searchBox.addListener('places_changed', () => {
+        const places = searchBox.getPlaces()
+        console.log('places')
+        console.log(places)
+
+        if (places.length === 0) {
+          return
+        }
+
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds()
+        const place = places[0]
+        this.myLat = place.geometry.location.lat()
+        this.myLong = place.geometry.location.lng()
+
+        myMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng()),
+          draggable: true,
+          title: place.name,
+        })
+        google.maps.event.addListener(myMarker, 'dragend', evt => {
+          this.myLat = evt.latLng.lat()
+          this.myLong = evt.latLng.lng()
+          map.panTo(evt.latLng)
+        })
+
+        // myMarker.position = new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng())
+        // myMarker.title = place.name
+
+        map.panTo(place.geometry.location)
+        map.setCenter(myMarker.position)
+        myMarker.setMap(map)
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport)
+        } else {
+          bounds.extend(place.geometry.location)
+        }
+        map.fitBounds(bounds)
+      })
+    },
+    async getMyCurrentPosition() {
       navigator.geolocation.getCurrentPosition(this.createCurrentPosition, this.showError)
     },
     showError() {
@@ -144,17 +220,14 @@ export default {
       })
     },
     updatePosition() {
-      console.log('this.center')
-      console.log(this.center)
-      console.log(this.markerLatLng)
       const config = {
         method: 'patch',
         url: buildServiceUrl(`/provider/${this.userId}`),
         headers: { },
         data: {
-          latitude: this.latitude === null ? 0.0 : this.latitude,
+          latitude: this.myLat === null ? 0.0 : this.myLat,
           altitude: this.altitude === null ? 0.0 : this.altitude,
-          longitude: this.longitude === null ? 0.0 : this.longitude,
+          longitude: this.myLong === null ? 0.0 : this.myLong,
         },
       }
 
@@ -185,5 +258,9 @@ export default {
   &.leaflet-container{
     height: 350px;
   }
+}
+
+#map {
+  height: 100%;
 }
 </style>
